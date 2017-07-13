38b170df1c511757aa12297243795d92f118a215
Dennis Luo <dennis.luo1@gmail.com>
2017-07-13 10:02:20 -0400

    Completed Phase -2 to Phase 1

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    // Give names to class to use for jQuery 
+    var container = $('.container');
+    var superman = $('.superman');
+    var column = $('.column');
+    var columnOne = $('#column-one);
+    var columnTwo = $('#column-two');
+    var score = $('.score');
+    var reset = $('.reset');
+
+    
+});
