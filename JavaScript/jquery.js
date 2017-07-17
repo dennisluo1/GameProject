25c4eb083ee9c02ff4b370a37b3d36d9232a28e3
Dennis Luo <dennis.luo1@gmail.com>
2017-07-16 23:25:37 -0400

    Completed Phase 5

--- JavaScript/jquery.js
+++ JavaScript/jquery.js
@@ -1,4 +1,4 @@
-// Understanding logic of flappy bird: https://www.youtube.com/watch?v=cXgA1d_E-jY&t=3s
+// Understanding logic of flappy bird from: https://www.youtube.com/watch?v=cXgA1d_E-jY&t=3s
 $(function () {
     // Give names to class and id to use for dom manipulation/jQuery
     var container = $('.container');
@@ -22,30 +22,29 @@ $(function () {
     var fallUp = false;
     // This is to get the columns moving
     var beginGame = setInterval(function() {
-            //top column // bottom column // top of container // bottom of container
-        if ( collisionDetector(superman, columnOne) ||  collisionDetector(superman, columnTwo) || parseInt(superman.css('top') <= 0 ) || 
-            parseInt(superman.css('top')) > containerHeight - supermanHeight) 
+        // Top column // bottom column // top of container // bottom of container
+        if ( collisionDetector(superman, columnOne) || collisionDetector(superman, columnTwo) || parseInt(superman.css('top')) > containerHeight - supermanHeight)
             {
             // Once collision is detected, end the game 
             endGame();
         } else {
-        //Create variable to take value of columnCurrentPosition 
+        // Create variable to take value of columnCurrentPosition 
         var columnCurrent = parseInt(column.css('right'));
-
-        //Update score 
+        // Update score by 1 everytime superman flies past columns and columns disappear
         if(supermanRight < columnCurrent) {
             // columnCurrent > containerWidth - supermanLeft
             score.text(parseInt(score.text()) + 1)
         }
-        //Create if statement, once columns leave, need to return
-        // update the scores
+        // Create if statement, once columns leave, need to return
+        // Update the scores
         if(columnCurrent > containerWidth ) {
-            // Randomize column heights
-            var changeHeight = parseInt(Math.random() * 10);
+            // Randomize column heights top and bottom
+            var changeHeightOne = parseInt(Math.random() * 200); 
+            var changeHeightTwo = parseInt(Math.random() * 120);
             // Manipulate height of first column
-            columnOne.css('height', columnCurrentPosition + changeHeight);
+            columnOne.css('height', columnCurrentPosition + changeHeightOne);
             // Manipulate height of second column
-            columnTwo.css('height', columnCurrentPosition - changeHeight);
+            columnTwo.css('height', columnCurrentPosition - changeHeightTwo);
             // Increase speed by 1 as game goes on
             currentSpeed  = currentSpeed + 1;
             speed.text(currentSpeed);
@@ -65,7 +64,6 @@ $(function () {
 function fallDown() {
     superman.css('top', parseInt(superman.css('top')) + 7);
 }
-// Referenced keyup/keydown from: https://codepen.io/JTParrett/pen/zwhy
 // Code to move Superman
 setInterval(flyingHero, 10);
 var sky = {} 
@@ -74,7 +72,6 @@ var forward = 10;
 $(window).keydown(function(event) {
     sky[event.keyCode] = true;
 });
-
 $(window).keyup(function(event) {
     delete sky[event.keyCode];
 });
@@ -87,7 +84,6 @@ function flyingHero() {
         if (direction == 38) { // up arrow
             $(".superman").animate({top: "-=5"}, 0); 
         }        
-
         if (direction == 39) { // right arrow
             $(".superman").animate({left: "+=5"}, 0); 
         }
@@ -101,17 +97,16 @@ function endGame() {
     clearInterval(beginGame);
     alert("Game Over! Play again?");
 }
-
 // Collision Detector
 // Referenced collision detector from: https://www.youtube.com/watch?v=Wrzr-QBVNNM&t=2393s
 // Top column, bottom column, top of container, bottom of container
 function collisionDetector($clark, $lex) {
-    // use offset to target top and left properties
+    // Use offset to target top and left properties
     var a = $clark.offset().left; 
     var b = $clark.offset().top;
     var c = $lex.offset().left;
     var d = $lex.offset().top;    
-    // use outHeight to target heights of element such as top and bottom padding
+    // Use outHeight to target heights of element such as top and bottom padding
     var heightOne = $clark.outerHeight(true);  
     var heightTwo = $lex.outerHeight(true);
     var widthOne = $clark.outerWidth(true);
@@ -121,13 +116,19 @@ function collisionDetector($clark, $lex) {
     var combineTwo = a + widthOne;
     var combineThree = d + heightTwo;
     var combineFour = c + widthTwo;
-    // if statements
+    // If statements
     if(combineOne < d || combineTwo < c || combineThree < b || combineFour < a) return false     ;
     return true;
 } 
 });
-// Reset with message and stop game permanently 
+// Reset the game
+$( ".reset" ).click(function() {
+    location.reload();
+});
+
 // Sources: 
+// http://flappybird.io/
+// https://codepen.io/aamirafridi/pen/tGFKx
 // https://www.youtube.com/watch?v=cXgA1d_E-jY&t=3s
 // https://www.youtube.com/watch?v=Wrzr-QBVNNM&t=2393s
 // https://codepen.io/JTParrett/pen/zwhyI
